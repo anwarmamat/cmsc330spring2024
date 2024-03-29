15789d18bf7b9351a988d705787267ccdd9eb6d8
Anwar Mamat <anwarmamat@gmail.com>
2024-03-29 11:40:16 -0400

    updates rev and tests

--- examples/qcheck/rev/rev.ml
+++ examples/qcheck/rev/rev.ml
@@ -1,14 +1,22 @@
-(*try this example in UTOP *)
+(*If you try this example in utop, add 
 
 #require "qcheck";;
 
+*)
+
 open QCheck
 
-let reverse = List.rev;;
-
+(* Reverse a list *)
+let rec reverse lst = 
+   List.fold_left (fun acc x->x::acc) [] lst
+  
+(* two properties of the `reverse` function *)
 let prop_reverse l = reverse (reverse l) = l;;
 
-let test =
+let prop_reverse2 l1 m l2 =
+  reverse (l1 @ [m] @ l2) = reverse l2 @ [m] @ reverse l1
+
+let test1 =
   Test.make
   ~count:1000 
   ~name:"reverse_test" 
@@ -16,7 +24,17 @@ let test =
  (fun x-> prop_reverse x)
 ;;
 
- QCheck_runner.run_tests ~verbose:true [test];;
+let test2 =
+  Test.make
+  ~count:1000 
+  ~name:"reverse_test" 
+  QCheck.(triple (list small_int) small_int (list small_int))
+  (fun (l1, m, l2)-> prop_reverse2 l1 m l2 )
+;;
+
+ QCheck_runner.run_tests 
+   ~verbose:true 
+  [test1; test2];;
 
 
                                                              
